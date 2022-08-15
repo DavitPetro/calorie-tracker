7bf616dcc800d7146b0d8cff98cc5a5e023c1652
Davit Petrosyan <64999212+DavitPetro@users.noreply.github.com>
2022-08-15 20:46:36 +0000

    Trying out Fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+    Filename: App.js
+    Author: Davit Petrosyan
+    Description: Main logical component of the calorie tracker app. 
+    It uses the API functionality for finding nutritional info about the queried item.
+*/
+import './Style.css';
 
 function App() {
+
+  const parameters = {
+    key: 'YKwPgkH8koOMHmcoOfafT8u75nOEQMrz44Ab42y2',
+    query: 'apple',
+    dataType: ["Survey (FNDDS)"],
+    pageSize: 5,
+  }
+
+  async function componentDidMount() {
+    const apiURL = `https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=${encodeURIComponent(parameters.key)}
+    &query=${encodeURIComponent(parameters.query)}
+    &dataType=${encodeURIComponent(parameters.dataType)}
+    &pageSize=${encodeURIComponent(parameters.pageSize)}`
+    const response = await fetch(apiURL);
+    const data = response.json()
+    console.log(data)   
+ }
+
   return (
     <div id="content">
     </div>
